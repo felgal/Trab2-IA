d252ac53f934fdd92051372639dbf5af077f8919
felgal <rag.felipe@gmail.com>
2019-06-07 20:08:44 -0300

    Entrada de dados e modelo padrão de pergunta

--- ia.py
+++ ia.py
@@ -45,9 +45,32 @@ def printaGrafo(verts):
             for z in y.relacoes:
                 print("Relacao da aresta: "+str(z))
         for y in x.valores:
-            print("Nome valor: "+y)
+            print("Nome valor: "+str(y))
         print()
 
+def verificaExisteRecursivo(vertice,vertAnt,nome,profundidade):
+    if profundidade==0:
+        return ""
+    #print(vertice.nome+"  "+nome)
+    oceanos=""
+    for x in vertice.arestas:
+        if(x.vertices[0].nome=="oceano" or x.vertices[1].nome=="oceano" ):
+            for y in x.relacoes:
+                if(y[0]==nome):
+                    return " "+y[1]
+                if(y[1]==nome):
+                    return " "+y[0]
+        if(x.vertices[0].nome!=vertAnt and x.vertices[1].nome!=vertAnt):
+            for y in x.relacoes:
+                if(y[0]==nome and x.vertices[0].nome==vertice.nome):
+                    oceanos +=verificaExisteRecursivo(x.vertices[1],vertice.nome,y[1],profundidade-1)
+                if(y[1]==nome and x.vertices[0].nome==vertice.nome):
+                    oceanos +=verificaExisteRecursivo(x.vertices[1],vertice.nome,y[0],profundidade-1)
+                if(y[0]==nome and x.vertices[1].nome==vertice.nome):
+                    oceanos +=verificaExisteRecursivo(x.vertices[0],vertice.nome,y[1],profundidade-1)
+                if(y[1]==nome and x.vertices[1].nome==vertice.nome):
+                    oceanos +=verificaExisteRecursivo(x.vertices[0],vertice.nome,y[0],profundidade-1)
+    return oceanos
 
 vertices=[]
 arestas=[]
@@ -82,7 +105,18 @@ for linha in linhas:
     linha = linha.split("(")
     linha[1] = linha[1].strip(' ').split(",")
 
-    if(linha[0]=="governo"):
+    if(linha[0]=="governo" or linha[0]=="moeda" or linha[0]=="continente" or linha[0]=="area" or linha[0]=="populacao" or linha[0]=="capital" or linha[0]=="idioma" or linha[0]=="colonia" or linha[0]=="pib" or linha[0]=="oceano" or linha[0]=="clima" or linha[0]=="subcontinente" or linha[0]=="regiao_continental"):
+        arest = verificaSeExisteArestaPorNome(arestas,"pais",linha[0])
+        arest.relacoes.append(linha[1])
+        vert1 = verificaSeExiste(vertices,"pais")
+        if(not verificaSeExisteVal(vert1,linha[1][0])):
+            vert1.valores.append(linha[1][0])
+        vert2 = verificaSeExiste(vertices,linha[0])
+        if(not verificaSeExisteVal(vert2,linha[1][1])):
+            vert2.valores.append(linha[1][1])
+
+    if(linha[0]=="ilha"):
+        linha[1] = [linha[1],linha[1]]
         arest = verificaSeExisteArestaPorNome(arestas,"pais",linha[0])
         arest.relacoes.append(linha[1])
         vert1 = verificaSeExiste(vertices,"pais")
@@ -93,6 +127,26 @@ for linha in linhas:
             vert2.valores.append(linha[1][1])
 
 
+    if(linha[0]=="continentes"):
+        linha[0]="continente"
+        linha[1] = [linha[1],linha[1]]
+        arest = verificaSeExisteArestaPorNome(arestas,"subcontinente",linha[0])
+        arest.relacoes.append(linha[1])
+        vert1 = verificaSeExiste(vertices,"subcontinente")
+        if(not verificaSeExisteVal(vert1,linha[1][0])):
+            vert1.valores.append(linha[1][0])
+        vert2 = verificaSeExiste(vertices,linha[0])
+        if(not verificaSeExisteVal(vert2,linha[1][1])):
+            vert2.valores.append(linha[1][1])
+
 printaGrafo(vertices)
 
+#exemplo de pergunta para testar: quais oceanos encostam na america?
+#quero achar continente-banhado-oceano
+
+vertice = verificaSeExiste(vertices,"continente")
+mylist = verificaExisteRecursivo(vertice,"","america",6).split(" ")
+mylist = list(dict.fromkeys(mylist))
+mylist.pop(0)
+print(mylist)
 f.close()
